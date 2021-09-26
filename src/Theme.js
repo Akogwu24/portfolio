9aa1f3ad56ba30cde965e8bf2777fc9a8baf0064
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-09-26 19:39:17 +0100

    development environment set

---
+++ src/Theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    myColor: {
+      primary: '#0e0f63',
+      secondary: '#373869',
+      tatiary: '#FDD2BF',
+    },
+  },
+
+});
